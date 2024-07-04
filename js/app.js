3160ca2721291897ea356b30e9b389050330f7c8
Sanes05 <kevinmauricejordan2005@gmail.com>
2024-07-05 01:35:47 +0200

    Burger Menu Wie bei chefkoch gemacht

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
-function ToggleMenu() {
-	const menu = document.getElementById("burger-menu");
+document.addEventListener("DOMContentLoaded", function () {
+	const burgerMenuIcon = document.getElementById("burger-menu-icon");
+	const burgerMenu = document.getElementById("burger-menu");
+	const overlay = document.getElementById("overlay");
 
-	menu.classList.toggle("show-burger-menu");
-}
+	// Öffnen und Schließen des Burger-Menüs
+	burgerMenuIcon.addEventListener("click", function () {
+		burgerMenu.classList.toggle("active");
+		overlay.classList.toggle("active");
+	});
+
+	// Schließen des Menüs beim Klicken auf das Overlay
+	overlay.addEventListener("click", function () {
+		burgerMenu.classList.remove("active");
+		overlay.classList.remove("active");
+	});
+});
