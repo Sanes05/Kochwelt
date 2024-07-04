14c3513f13953d71ccda81928eadf9eb5480b872
Sanes05 <kevinmauricejordan2005@gmail.com>
2024-07-04 03:37:03 +0200

    Validation border color Hinzugefügt

--- kontakt/script.js
+++ kontakt/script.js
@@ -7,16 +7,24 @@ function FormValidation() {
 		document.getElementById("error-message-email"),
 		document.getElementById("error-message-message"),
 	];
+	const ErrorChangeBorderColor = [
+		document.getElementById("name"),
+		document.getElementById("email"),
+		document.getElementById("message"),
+	];
 	if (Name == "") {
 		Error[0].innerHTML =
-			"<p class=error-message >Bitte Gebe Einen Namen An</p>";
+			"<p class=error-message >Bitte gebe einen Namen ein</p>";
+		ErrorChangeBorderColor[0].classList.add("error-border");
 		return false;
 	} else if (Email == "") {
 		Error[1].innerHTML = "<p class=error-message >Bitte Gebe Eine Email An</p>";
+		ErrorChangeBorderColor[1].classList.add("error-border");
 		return false;
 	} else if (Message == "") {
 		Error[2].innerHTML =
 			"<p class=error-message >Bitte Füge Noch eine nachricht hinzu</p>";
+		ErrorChangeBorderColor[2].classList.add("error-border");
 		return false;
 	}
 }
