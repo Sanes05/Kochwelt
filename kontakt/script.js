edab5e7211ba20ec5077febd1c7b0b230aa8c158
Sanes05 <kevinmauricejordan2005@gmail.com>
2024-07-04 03:27:25 +0200

    Valiedirung in html angezeigt

--- kontakt/script.js
+++ kontakt/script.js
@@ -2,15 +2,22 @@ function FormValidation() {
 	const Name = document.forms["MyForm"]["name"].value;
 	const Email = document.forms["MyForm"]["email"].value;
 	const Message = document.forms["MyForm"]["message"].value;
+	const Error = [
+		document.getElementById("error-message-name"),
+		document.getElementById("error-message-email"),
+		document.getElementById("error-message-message"),
+	];
 
 	if (Name == "") {
-		alert("Name Muss Ausgefült werden");
+		Error[0].innerHTML =
+			"<p class=error-message >Bitte Gebe Einen Namen An</p>";
 		return false;
 	} else if (Email == "") {
-		alert("Email Muss Ausgefült werden");
+		Error[1].innerHTML = "<p class=error-message >Bitte Gebe Eine Email An</p>";
 		return false;
 	} else if (Message == "") {
-		alert("Es fehlt Noch eine nachricht");
+		Error[2].innerHTML =
+			"<p class=error-message >Bitte Füge Noch eine nachricht hinzu</p>";
 		return false;
 	}
 }
