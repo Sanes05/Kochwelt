aed7b751e341e5b950c26be98751d60d2dafc12d
Sanes05 <kevinmauricejordan2005@gmail.com>
2024-07-04 03:06:50 +0200

    Kontaktformular Valiedierung Hinzugefügt

---
+++ kontakt/script.js
@@ -0,0 +1,16 @@
+function FormValidation() {
+	const Name = document.forms["MyForm"]["name"].value;
+	const Email = document.forms["MyForm"]["email"].value;
+	const Message = document.forms["MyForm"]["message"].value;
+
+	if (Name == "") {
+		alert("Name Muss Ausgefült werden");
+		return false;
+	} else if (Email == "") {
+		alert("Email Muss Ausgefült werden");
+		return false;
+	} else if (Message == "") {
+		alert("Es fehlt Noch eine nachricht");
+		return false;
+	}
+}
