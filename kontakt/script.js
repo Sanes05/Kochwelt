bb38d5f8330b4ca0f0488f45cf3da72454ed0511
Kevin Jordan <Kevinmauricejordan2005@gmail.com>
2024-07-04 14:00:43 +0200

    Mail Validator Hinzu Gefügt

--- kontakt/script.js
+++ kontakt/script.js
@@ -1,7 +1,17 @@
 function FormValidation() {
+	const Document = document.forms["MyForm"];
 	const Name = document.forms["MyForm"]["name"].value;
 	const Email = document.forms["MyForm"]["email"].value;
 	const Message = document.forms["MyForm"]["message"].value;
+	const FormSpreeKey = "https://formspree.io/f/xblrjkbr";
+	let form = document.getElementById("MyForm");
+
+	const isValidEmail = (Email) => {
+		const re =
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(String(Email).toLowerCase());
+	};
+
 	const Error = [
 		document.getElementById("error-message-name"),
 		document.getElementById("error-message-email"),
@@ -21,10 +31,18 @@ function FormValidation() {
 		Error[1].innerHTML = "<p class=error-message >Bitte Gebe Eine Email An</p>";
 		ErrorChangeBorderColor[1].classList.add("error-border");
 		return false;
+	} else if (!isValidEmail(Email)) {
+		Error[1].innerHTML =
+			"<p class=error-message >Bitte Gebe Eine Gültiege Email an Email An</p>";
+		ErrorChangeBorderColor[1].classList.add("error-border");
+		return false;
 	} else if (Message == "") {
 		Error[2].innerHTML =
 			"<p class=error-message >Bitte Füge Noch eine nachricht hinzu</p>";
 		ErrorChangeBorderColor[2].classList.add("error-border");
 		return false;
+	} else {
+		form.action = FormSpreeKey;
+		form.method = "POST";
 	}
 }
