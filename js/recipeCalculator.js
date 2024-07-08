8a616dddb1df22975045c51176f03e05b188e0fc
Kevin Jordan <Kevinmauricejordan2005@gmail.com>
2024-07-08 20:22:35 +0200

    Js Verbesserungen

--- js/recipeCalculator.js
+++ js/recipeCalculator.js
@@ -9,47 +9,22 @@ function CalculatePortions(id) {
 		error.innerHTML = "Portionen können nicht Über 20 sein";
 		errorBorder.classList.add("error-border");
 	} else if (id === "Kevin") {
-		CalculatePortionsKevin();
+		CalculateIngredients("Kevin", (menge = [400, 100, 3, 200, 50, 1]));
 	} else if (id === "Thomas") {
-		CalculatePortionsThomas();
+		CalculateIngredients("Thomas", (menge = [250, 1, 100, 80, 50, 4, 2]));
 	} else if (id === "Tobias") {
-		CalculatePortionsTobias();
+		CalculateIngredients("Tobias", (menge = [150, 50, 100, 1, 6, 5, 3]));
 	} else {
 		console.error("Keine ID");
 	}
 }
-
-function CalculatePortionsKevin() {
+function CalculateIngredients(id, menge = []) {
+	const portionen = document.getElementById("zutaten").value;
 	const errorBorder = document.getElementById("zutaten");
 	const error = document.getElementById("error-message");
-	const portionen = document.getElementById("zutaten").value;
 	const num = 4;
-	const menge = [400, 100, 3, 200, 50, 1];
-	let Ingredient = [
-		document.getElementById("firtst-ingredient"),
-		document.getElementById("second-ingredient"),
-		document.getElementById("third-ingredient"),
-		document.getElementById("fourth-ingredient"),
-		document.getElementById("fifth-ingredient"),
-		document.getElementById("sixth-ingredient"),
-	];
-
 	error.innerHTML = "";
 	errorBorder.classList.remove("error-border");
-	Ingredient[0].innerText = `${(menge[0] / num) * portionen}`;
-	Ingredient[1].innerText = `${(menge[1] / num) * portionen}`;
-	Ingredient[2].innerText = `${(menge[2] / num) * portionen}`.replace(".", ",");
-	Ingredient[3].innerText = `${(menge[3] / num) * portionen}`;
-	Ingredient[4].innerText = `${(menge[4] / num) * portionen}`.replace(".", ",");
-	Ingredient[5].innerText = `${(menge[5] / num) * portionen}`.replace(".", ",");
-}
-
-function CalculatePortionsThomas() {
-	const portionen = document.getElementById("zutaten").value;
-	const errorBorder = document.getElementById("zutaten");
-	const error = document.getElementById("error-message");
-	const num = 4;
-	const menge = [250, 1, 100, 80, 50, 4, 2];
 	let Ingredient = [
 		document.getElementById("firtst-ingredient"),
 		document.getElementById("second-ingredient"),
@@ -66,48 +41,37 @@ function CalculatePortionsThomas() {
 		document.getElementById("thirteenth-ingredient"),
 	];
 
-	error.innerHTML = "";
-	errorBorder.classList.remove("error-border");
-	Ingredient[0].innerText = `${(menge[0] / num) * portionen}`.replace(".", ",");
-	Ingredient[1].innerText = `${(menge[1] / num) * portionen}`.replace(".", ",");
-	Ingredient[2].innerText = `${(menge[1] / num) * portionen}`.replace(".", ",");
-	Ingredient[3].innerText = `${(menge[1] / num) * portionen}`.replace(".", ",");
-	Ingredient[4].innerText = `${(menge[1] / num) * portionen}`.replace(".", ",");
-	Ingredient[5].innerText = `${(menge[2] / num) * portionen}`.replace(".", ",");
-	Ingredient[6].innerText = `${(menge[3] / num) * portionen}`;
-	Ingredient[7].innerText = `${(menge[4] / num) * portionen}`.replace(".", ",");
-	Ingredient[8].innerText = `${(menge[5] / num) * portionen}`;
-	Ingredient[9].innerText = `${(menge[4] / num) * portionen}`.replace(".", ",");
-	Ingredient[10].innerText = `${(menge[2] / num) * portionen}`;
-	Ingredient[11].innerText = `${(menge[4] / num) * portionen}`.replace(".", ",");
-	Ingredient[12].innerText = `${(menge[6] / num) * portionen}`.replace(".", ",");
-}
-
-function CalculatePortionsTobias() {
-	const errorBorder = document.getElementById("zutaten");
-	const error = document.getElementById("error-message");
-	const portionen = document.getElementById("zutaten").value;
-	const num = 4;
-	const menge = [150, 50, 100, 1, 6, 5, 3];
-	let Ingredient = [
-		document.getElementById("firtst-ingredient"),
-		document.getElementById("second-ingredient"),
-		document.getElementById("third-ingredient"),
-		document.getElementById("fourth-ingredient"),
-		document.getElementById("fifth-ingredient"),
-		document.getElementById("sixth-ingredient"),
-		document.getElementById("seventh-ingredient"),
-		document.getElementById("eighth-ingredient"),
-	];
-
-	error.innerHTML = "";
-	errorBorder.classList.remove("error-border");
-	Ingredient[0].innerText = `${(menge[0] / num) * portionen}`.replace(".", ",");
-	Ingredient[1].innerText = `${(menge[1] / num) * portionen}`.replace(".", ",");
-	Ingredient[2].innerText = `${(menge[2] / num) * portionen}`.replace(".", ",");
-	Ingredient[3].innerText = `${(menge[3] / num) * portionen}`.replace(".", ",");
-	Ingredient[4].innerText = `${(menge[3] / num) * portionen}`.replace(".", ",");
-	Ingredient[5].innerText = `${(menge[4] / num) * portionen}`.replace(".", ",");
-	Ingredient[6].innerText = `${(menge[5] / num) * portionen}`.replace(".", ",");
-	Ingredient[7].innerText = `${(menge[6] / num) * portionen}`.replace(".", ",");
+	if (id === "Kevin") {
+		Ingredient[0].innerText = `${(menge[0] / num) * portionen}`.replace(".", ",");
+		Ingredient[1].innerText = `${(menge[1] / num) * portionen}`.replace(".", ",");
+		Ingredient[2].innerText = `${(menge[2] / num) * portionen}`.replace(".", ",");
+		Ingredient[3].innerText = `${(menge[3] / num) * portionen}`.replace(".", ",");
+		Ingredient[4].innerText = `${(menge[4] / num) * portionen}`.replace(".", ",");
+		Ingredient[5].innerText = `${(menge[5] / num) * portionen}`.replace(".", ",");
+	} else if (id === "Thomas") {
+		Ingredient[0].innerText = `${(menge[0] / num) * portionen}`.replace(".", ",");
+		Ingredient[1].innerText = `${(menge[1] / num) * portionen}`.replace(".", ",");
+		Ingredient[2].innerText = `${(menge[1] / num) * portionen}`.replace(".", ",");
+		Ingredient[3].innerText = `${(menge[1] / num) * portionen}`.replace(".", ",");
+		Ingredient[4].innerText = `${(menge[1] / num) * portionen}`.replace(".", ",");
+		Ingredient[5].innerText = `${(menge[2] / num) * portionen}`.replace(".", ",");
+		Ingredient[6].innerText = `${(menge[3] / num) * portionen}`.replace(".", ",");
+		Ingredient[7].innerText = `${(menge[4] / num) * portionen}`.replace(".", ",");
+		Ingredient[8].innerText = `${(menge[5] / num) * portionen}`.replace(".", ",");
+		Ingredient[9].innerText = `${(menge[4] / num) * portionen}`.replace(".", ",");
+		Ingredient[10].innerText = `${(menge[2] / num) * portionen}`.replace(".", ",");
+		Ingredient[11].innerText = `${(menge[4] / num) * portionen}`.replace(".", ",");
+		Ingredient[12].innerText = `${(menge[6] / num) * portionen}`.replace(".", ",");
+	} else if (id === "Tobias") {
+		Ingredient[0].innerText = `${(menge[0] / num) * portionen}`.replace(".", ",");
+		Ingredient[1].innerText = `${(menge[1] / num) * portionen}`.replace(".", ",");
+		Ingredient[2].innerText = `${(menge[2] / num) * portionen}`.replace(".", ",");
+		Ingredient[3].innerText = `${(menge[3] / num) * portionen}`.replace(".", ",");
+		Ingredient[4].innerText = `${(menge[3] / num) * portionen}`.replace(".", ",");
+		Ingredient[5].innerText = `${(menge[4] / num) * portionen}`.replace(".", ",");
+		Ingredient[6].innerText = `${(menge[5] / num) * portionen}`.replace(".", ",");
+		Ingredient[7].innerText = `${(menge[6] / num) * portionen}`.replace(".", ",");
+	} else {
+		console.error("Falsche oder Keine id");
+	}
 }
